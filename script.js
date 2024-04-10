bb16f07224254aff731bf982a097ee5cc48eff59
kashyapsurani <kashyapsurani49@gmail.com>
2024-04-10 10:25:28 +0530

    Javascript_First_Project

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.getElementById("demo").innerHTML = `<h1>Kashyap surani </h1>
     <li>Bootsrap</li>
     <li>jQuery</li>
 </ul>
+
 <ol>
     <li>
         <h4>Protfolio Website</h4>
@@ -18,7 +19,10 @@ document.getElementById("demo").innerHTML = `<h1>Kashyap surani </h1>
     </li>
     <p>An interactive web app for managing tasks and to-do lists.</p>
 </ol>
+
 <h2> Contact</h2>
+
 <p>You can reach me at:<a href="mailto:kashyapsurani49@gmail.com">Gmail</a></p>
+
 <p>© 2023 kashyapsurani. All rights reserved.</p>`;
 
