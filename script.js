cbbf27019c3bd49023754e9e7eea8a43b91aa625
kashyapsurani <kashyapsurani49@gmail.com>
2024-04-10 10:16:50 +0530

    Javascript_First_Project

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-document.write("<h1>Kashyap surani </h1> ")
-document.write("<h2>Fullstack devlopment </h2>")
-document.write("<p>Welcome To My Protfolio I Am In Red And White</p>")
-document.write("<h3>Skills</h3>")
-document.write("<ul><li>Html</li>   <li>Css</li>  <li>Bootsrap</li>  <li>jQuery</li></ul>")
-document.write("<ol><li> <h4>Protfolio Website</h4></li> <p>A personal portfolio website showcasing my skills and projects.</p>  <li> <h4>2. Task Management App</h4></li> <p>An interactive web app for managing tasks and to-do lists.</p></ol>")
-document.write("<h2>Contact</h2>")
-document.write(`<p>You can reach me at:<a href="mailto:kashyapsurani49@gmail.com">Gmail</a></p>`)
-document.write("<p>© 2023 kashyapsurani. All rights reserved.</p>")
+document.getElementById("demo").innerHTML = `<h1>Kashyap surani </h1>
+<h2>Fullstack devlopment </h2>
+<p>Welcome To My Protfolio I Am In Red And White</p>
+<h3>Skills</h3>
+<ul>
+    <li>Html</li>
+    <li>Css</li>
+    <li>Bootsrap</li>
+    <li>jQuery</li>
+</ul>
+<ol>
+    <li>
+        <h4>Protfolio Website</h4>
+    </li>
+    <p>A personal portfolio website showcasing my skills and projects.</p>
+    <li>
+        <h4>2. Task Management App</h4>
+    </li>
+    <p>An interactive web app for managing tasks and to-do lists.</p>
+</ol>
+<h2> Contact</h2>
+<p>You can reach me at:<a href="mailto:kashyapsurani49@gmail.com">Gmail</a></p>
+<p>© 2023 kashyapsurani. All rights reserved.</p>`;
